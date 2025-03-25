f3e00f5f2b71cc721db518c7ff4185b052a1be6b
vky5 <vky05@proton.me>
2025-03-25 15:07:46 +0530

    fixed slide issue

--- src/components/HorizontalScroll.tsx
+++ src/components/HorizontalScroll.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import HomePage from "./HomeSlide";
+
 function HorizontalScroll() {
   return (
     <div className="flex w-full h-full">
@@ -12,37 +14,26 @@ function HorizontalScroll() {
           <li>Skills</li>
         </ul>
       </section>
-
-      {/* this section is going to contain all the slides */}
-      {/* <section className="flex justify-center items-center w-3/4 h-full overflow-x-auto bg-green-400 space-x-3"> */}
-        {/* but let's create a section which is going to be the center position of slideshow */}
-        {/* <section className="min-w-[75%] h-3/4 bg-red-500">
-          this is the text
-        </section>
-        <section className="min-w-[75%] h-3/4 bg-red-500">
-          this is another text
-        </section> */}  
-        {/* <div className="w-3/4 h-3/4 bg-green-500 overflow-x-visible space-x-3 flex z-10">
-          <section className="min-w-full h-full bg-red-500">
-            this is the text
-          </section>
-          <section className="min-w-full h-full bg-red-500"> 
-            this is another text
-          </section>
-        </div>
-      </section> */}
-      
-
-      <section className="w-full overflow-x-auto flex space-x-5 justify-start items-center">
-          <div className="min-w-3/4 h-4/5 flex items-center justify-between bg-amber-200">this is text</div>
-          <div className="min-w-3/4 h-4/5 flex items-center justify-between bg-amber-200">this is text</div>
-          <div className="min-w-3/4 h-4/5 flex items-center justify-between bg-amber-200">this is text</div>
-          <div className="min-w-3/4 h-4/5 flex items-center justify-between bg-amber-200">this is text</div>
-          <div className="min-w-3/4 h-4/5 flex items-center justify-between bg-amber-200">this is text</div>
+      <section className="w-full overflow-x-auto flex space-x-5 justify-start items-center px-10" style={{scrollbarWidth: "none"}}>
+        {[
+          <HomePage key="home-page" />,
+          "This is the section",
+          "This is the section",
+          "This is the section",
+          "This is the section",
+        ].map((content, index, arr) => (
+          <div
+            key={index}
+            className={`min-w-3/4 h-full flex items-center justify-end  ${
+              `${index === arr.length - 1 ? "mr-36" : ""} ${index === 0 ? "ml-10" : ""}` // Extra margin for last and first slides
+            }`}
+          >
+            <div className="w-4xl h-10/12 bg-red-500 flex items-center justify-center">
+              {content}
+            </div>
+          </div>
+        ))}
       </section>
-
-
-
     </div>
   );
 }
